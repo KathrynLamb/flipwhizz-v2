653ec7a670b8ad817860ff899989538217158787
kathrynlamb <katylamb2000@gmail.com>
2026-01-28 18:32:52 +0000

    checking bucket looking in firebase

--- src/lib/firebase-admin.node.ts
+++ src/lib/firebase-admin.node.ts
@@ -1,29 +1,25 @@
 import admin from "firebase-admin";
 
 const base64 = process.env.FIREBASE_ADMIN_CREDENTIALS_BASE64;
-const storageBucket = process.env.FIREBASE_STORAGE_BUCKET;
 
 if (!base64) {
   throw new Error("Missing FIREBASE_ADMIN_CREDENTIALS_BASE64");
-}
-
-
-
-if (!storageBucket) {
-  throw new Error("Missing FIREBASE_STORAGE_BUCKET");
 }
 
 const credentials = JSON.parse(
   Buffer.from(base64, "base64").toString("utf-8")
 );
 
+if (!credentials.storage_bucket) {
+  throw new Error("Service account JSON missing storage_bucket");
+}
+
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert(credentials),
-    storageBucket,
+    storageBucket: credentials.storage_bucket,
   });
 }
 
-export const adminStorage = admin.storage().bucket(storageBucket);
-console.log("🔥 Firebase bucket:", adminStorage.name);
+export const adminStorage = admin.storage().bucket();
 export default admin;
