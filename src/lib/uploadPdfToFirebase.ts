c1fb714afe2efb4c1b7a71dd3598ea55754cf4cf
kathrynlamb <katylamb2000@gmail.com>
2026-02-27 14:46:42 +0000

    upload pdf to firebase public vars

--- src/lib/uploadPdfToFirebase.ts
+++ src/lib/uploadPdfToFirebase.ts
@@ -6,11 +6,11 @@ function getFirebaseApp(): App {
 
   return initializeApp({
     credential: cert({
-      projectId: process.env.FIREBASE_PROJECT_ID!,
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL!,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+      projectId: process.env.FIREBASE_ADMIN_PROJECT_ID!,
+      clientEmail: process.env.FIREBASE_ADMIN_CLIENT_EMAIL!,
+      privateKey: process.env.FIREBASE_ADMIN_PRIVATE_KEY?.replace(/\\n/g, "\n"),
     }),
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET!,
+    storageBucket: process.env.FIREBASE_ADMIN_STORAGE_BUCKET!,
   });
 }
 
