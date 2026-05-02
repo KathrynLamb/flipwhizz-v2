d00b10f631c0c201ff32ec7d37ee79ef24337079
kathrynlamb <katylamb2000@gmail.com>
2026-05-02 16:34:56 +0100

    sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -2,7 +2,7 @@ import { MetadataRoute } from "next";
 import { getAllPosts } from "@/lib/blog";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = "https://www.flipwhizz.com";
+  const baseUrl = "https://flipwhizz.com"; // removed www
 
   const staticPages: MetadataRoute.Sitemap = [
     {
